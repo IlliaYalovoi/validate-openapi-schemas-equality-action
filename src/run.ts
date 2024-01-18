0f57a4fffdaee32a14fb070d37b0936b0cab1f93
Illia Yalovoi <imperio091@gmail.com>
2024-01-18 10:11:00 +0200

    GitHub action setup

--- src/run.ts
+++ src/run.ts
@@ -1,39 +1,27 @@
+import * as core from "@actions/core"
+
 import {parseOpenAPIV3SpecFiles} from "./parser";
 import {validateSchemas} from "./checker";
 import {getAllFilesInFolder} from "./fs"
 
-// const files : string[] = [
-//     "./api/auditlog.yaml",
-//     "./api/codewars.yaml",
-//     "./api/grading.yaml",
-//     "./api/inventory.yaml",
-//     "./api/notification.yaml",
-//     "./api/rbac.yaml",
-//     "./api/search.yaml",
-//     "./api/syllabus.yaml",
-//     "./api/user-management.yaml"
-// ]
-
-const path = './api'
-
 export async function run() {
     try {
+        const path = core.getInput("path")
         const files = getAllFilesInFolder(path)
 
         const apis = await parseOpenAPIV3SpecFiles(files)
-
         const validationErrors = validateSchemas(apis)
 
         if (validationErrors !== null) {
             validationErrors.forEach((error) => {
-                console.error(`Validation error for schema '${error.schemaKey}': ${error.error}`);
+                core.info(`Validation error for schema '${error.schemaKey}': ${error.error}`)
             })
+            core.setFailed('Some files failed validation')
         } else {
-            console.log('All schemas passed validation.')
+            core.info('All schemas passed validation!')
         }
-
     } catch (error) {
-        console.error('An error occurred:', error)
+        const formattedError = `An error occurred: ${error}`
+        core.setFailed(formattedError)
     }
-
 }
